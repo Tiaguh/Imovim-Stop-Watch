caf0025d2845f63ff79b2a3b25e157b598ab3b6f
Tiaguh <tiagosingup@gmail.com>
2023-02-18 00:32:39 -0300

    adjusting colors and waves

--- js/script.js
+++ js/script.js
@@ -1,22 +1,20 @@
-let result = document.getElementById("res")
-let result2 = document.getElementById("res2")
+let numberDays = document.getElementById("numberDays")
+let text = document.getElementById("text")
 
 function clock(){
   let data = new Date()
 
-  let initalDay = 13;
+  let startingDay = 13; // dia em que o projeto de 100 dias começou. (14/02/2023)
   let day = data.getDate();
 
-  let remainingDays = 100-(day - initalDay)
+  let remainingDays = 100-(day - startingDay)
 
-  result.innerHTML = remainingDays;
-  result2.innerHTML = "dias!"
-
-  console.log(remainingDays);
+  numberDays.innerHTML = remainingDays;
+  text.innerHTML = "dias!"
 
   if(remainingDays <= 0){
-    result.innerHTML = "Acabou o tempo!!!";
-    result2.innerHTML = ""
+    numberDays.innerHTML = "Acabou o tempo!!!";
+    text.innerHTML = ""
   }
 }
 
